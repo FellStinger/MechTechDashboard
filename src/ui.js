cd315618b5239a78ab80423da32dc059dd987937
FellStinger <shreyassmuthusamy@gmail.com>
2022-11-07 10:48:48 -0500

    Added basic timer js

--- src/ui.js
+++ src/ui.js
@@ -3,3 +3,8 @@ let ui = {
     timer: document.getElementById('timer'),
     robotState: document.getElementById('robot-state').firstChild
 }
+
+NetworkTables.addKeyListener('/robot/time', (key, value) => {
+    ui.timer.textContent = value < 0 ? '0:00' : Math.floor(value / 60) + ':' + (value % 60 < 10 ? '0' : '') + value % 60;
+    // Add color stuff here
+});
